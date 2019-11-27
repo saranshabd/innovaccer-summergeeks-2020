dff67cbc9f6e9a3e0a8e57a2fa97aa9355292605
Shabd Saran <shabd.179301181@muj.manipal.edu>
2019-11-28 03:26:41 +0530

    enable CORS

--- innovaccer/settings.py
+++ innovaccer/settings.py
@@ -35,17 +35,21 @@ INSTALLED_APPS = [
     # external apps
     'rest_framework',
     'boto3',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'innovaccer.urls'
 
